02a6d578b4f853b58f141642d8944d2c91e524dd
zevans98 <zman6221998@gmail.com>
2019-01-08 17:10:21 -0700

    new layout

---
+++ index.js
@@ -0,0 +1,8 @@
+$("#pic").on('clicl',
+    function() {
+        $(this).attr("src", "animate1.gif");
+    },
+    function() {
+        $(this).attr("src", "static1.jpg");
+    }                         
+);  
